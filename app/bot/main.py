1522e93776007bd7c5d096ce38a6471c1774075b
maxkolt <12345kolt@gmail.com>
2025-01-25 16:14:34 +0300

    initial

--- app/bot/main.py
+++ app/bot/main.py
@@ -17,30 +17,36 @@ keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(button_get_data)
 
 @dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
-    await message.answer("Welcome! Use the button below to get product data.", reply_markup=keyboard)
+    await message.answer("Добро пожаловать! Используйте кнопку ниже, чтобы получить данные о продукте.", reply_markup=keyboard)
 
 @dp.message_handler(lambda message: message.text == "Get Product Data")
 async def get_data_handler(message: types.Message):
-    await message.answer("Please enter the product artikul:")
+    await message.answer("Пожалуйста, введите артикул продукта:")
 
 @dp.message_handler()
 async def fetch_product_data(message: types.Message):
-    artikul = message.text
-    async with async_session() as session:
-        async with session.begin():
-            # Используем select для выполнения запроса
-            result = await session.execute(select(Product).where(Product.artikul == artikul))
-            product = result.scalars().first()
-            if product:
-                response = (f"Product Data:\n"
-                            f"Name: {product.name}\n"
-                            f"Price: {product.price} RUB\n"
-                            f"Rating: {product.rating}\n"
-                            f"Stock Quantity: {product.stock_quantity}")
-                await message.answer(response)
-            else:
-                await message.answer("Product not found.")
+    artikul = message.text.strip()
+    if not artikul.isalnum():
+        await message.answer("Неверный артикул. Пожалуйста, введите правильный артикул (только буквы и цифры).")
+        return
 
+    try:
+        async with async_session() as session:
+            async with session.begin():
+                result = await session.execute(select(Product).where(Product.artikul == artikul))
+                product = result.scalars().first()
+                if product:
+                    response = (f"Product Data:\n"
+                                f"Name: {product.name}\n"
+                                f"Price: {product.price} RUB\n"
+                                f"Rating: {product.rating}\n"
+                                f"Stock Quantity: {product.stock_quantity}")
+                    await message.answer(response)
+                else:
+                    await message.answer("Продукт не найден.")
+    except Exception as e:
+        await message.answer("При обработке вашего запроса произошла ошибка.")
+        print(f"Error: {e}")
 # Main entry point for the bot
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
